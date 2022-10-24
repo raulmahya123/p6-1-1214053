1cd492c14a33334d5fc52bff1e6c617532614e83
Mahyaaa <94241597+raulmahya123@users.noreply.github.com>
2022-10-24 15:22:04 +0700

    Add files via upload

---
+++ p6-2-1214053/p6-2-1214053/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace p6_2_1214053
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Visualbasic = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(476, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "C#";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Visualbasic
+            // 
+            this.Visualbasic.Location = new System.Drawing.Point(246, 114);
+            this.Visualbasic.Name = "Visualbasic";
+            this.Visualbasic.Size = new System.Drawing.Size(100, 20);
+            this.Visualbasic.TabIndex = 0;
+            this.Visualbasic.Text = "VisualBasic";
+            this.Visualbasic.TextChanged += new System.EventHandler(this.Visualbasic_TextChanged);
+            this.Visualbasic.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Visualbasic_MouseMove);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(39, 396);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Close";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(223, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(399, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Wich do you think is batter,Visaul basic or C#?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Yellow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.Visualbasic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox Visualbasic;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ p6-2-1214053/p6-2-1214053/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p6_2_1214053
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Visualbasic_TextChanged    (object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Corret");
+        }
+
+        private void Visualbasic_MouseMove(object sender, MouseEventArgs e)
+        {
+            Visualbasic.Top -= e.Y;
+            Visualbasic.Left -= e.X;
+            if (Visualbasic.Top < -16 || Visualbasic.Top > 160)
+                Visualbasic.Top = 73;
+            if (Visualbasic.Left < -64 || Visualbasic.Left > 384)
+                Visualbasic.Left = 160;
+        }
+    }
+}
